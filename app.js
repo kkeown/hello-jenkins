10ee6c361cbcc77898be281da9d2074d53318f7c
Karry Keown <kkeown@oc4567170058.ibm.com>
2015-02-27 16:17:18 -0700

    Switch to softcloud

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-    res.send('hello jenkins softcloud private');
+    res.send('hello jenkins softcloud private 3');
 });
  
 app.listen(process.env.PORT || 5000);
